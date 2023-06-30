b85b18d3c61ea6c966df658ce8918aa16b50e0c4
Fabs <fabien.michel@hespul.org>
2023-06-30 13:38:33 +0200

    fix: change y_axis to more real z_axis

--- obj_types/sash.py
+++ obj_types/sash.py
@@ -4,7 +4,7 @@ from dataclasses import dataclass
 @dataclass
 class Sash:
     x_axis: float = 0
-    y_axis: float = 0
+    z_axis: float = 0
     width: float = 0
     start_angle: float = 0
     end_angle: float = 0
